e950791338b927272a6f28c5c88cd577408c7954
Ivan <61393936+Sudo-Ivan@users.noreply.github.com>
2023-04-04 19:54:17 -0500

    add(core): PDF boxes & dragging

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,80 +1,96 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { PDFDocument } from 'pdf-lib';
 import styles from '@/styles/Home.module.css';
 
 const PDFHome: React.FC = () => {
   const [pdfs, setPdfs] = useState<File[]>([]);
+  const [thumbnails, setThumbnails] = useState<string[]>([]);
   const [mergedPdfName, setMergedPdfName] = useState<string>('merged.pdf');
-
-  const handlePDFInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const draggedItem = useRef<number | null>(null);
+  
+  const handlePDFInputChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const { files } = e.target;
     if (files && files.length > 0) {
-      setPdfs((prevPdfs) => [...prevPdfs, ...files]);
+      const newPdfs = Array.from(files);
+      setPdfs((prevPdfs) => [...prevPdfs, ...newPdfs]);
     }
   };
-
-  const handleSortPDFs = () => {
-    setPdfs((prevPdfs) =>
-      [...prevPdfs].sort((a, b) => a.name.localeCompare(b.name))
-    );
-  };
-
-  const handleRemovePDF = (index: number) => {
-    setPdfs((prevPdfs) => {
-      const newPdfs = [...prevPdfs];
-      newPdfs.splice(index, 1);
-      return newPdfs;
-    });
-  };
-
-  const handleMergedPdfNameChange = (
-    e: React.ChangeEvent<HTMLInputElement>
-  ) => {
+  
+  const handleMergedPdfNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setMergedPdfName(e.target.value);
   };
 
-  const handleMergePDFs = async () => {
-    const mergedPdf = await PDFDocument.create();
-    for (const pdf of pdfs) {
-      const fileReader = new FileReader();
-      const pdfBytes = await new Promise<Uint8Array>((resolve) => {
-        fileReader.onload = () => {
-          const bytes = new Uint8Array(fileReader.result as ArrayBuffer);
-          resolve(bytes);
-        };
-        fileReader.readAsArrayBuffer(pdf);
-      });
-      const pdfDoc = await PDFDocument.load(pdfBytes);
-      const copiedPages = await mergedPdf.copyPages(
-        pdfDoc,
-        pdfDoc.getPageIndices()
-      );
-      copiedPages.forEach((page) => mergedPdf.addPage(page));
-    }
-    const mergedPdfBytes = await mergedPdf.save();
-    const blob = new Blob([mergedPdfBytes], { type: 'application/pdf' });
-    const link = document.createElement('a');
-    link.href = window.URL.createObjectURL(blob);
-    link.download = mergedPdfName;
-    link.click();
+  const handleRemovePDF = (index: number) => {
+    setPdfs((prevPdfs) => prevPdfs.filter((_, i) => i !== index));
+    setThumbnails((prevThumbnails) => prevThumbnails.filter((_, i) => i !== index));
   };
 
+  const handleSortPDFs = () => {
+    const sortedPdfs = [...pdfs].sort((a, b) => a.name.localeCompare(b.name));
+    const sortedThumbnails = sortedPdfs.map((pdf) => {
+      const index = pdfs.findIndex((originalPdf) => originalPdf.name === pdf.name);
+      return thumbnails[index];
+    });
+  
+    setPdfs(sortedPdfs);
+    setThumbnails(sortedThumbnails);
+  };
+
+  const handleMergePDFs = async () => {
+    const mergedPdfDoc = await PDFDocument.create();
+    for (const pdf of pdfs) {
+      const pdfBytes = await pdf.arrayBuffer();
+      const sourcePdfDoc = await PDFDocument.load(pdfBytes);
+      const sourcePageIndices = sourcePdfDoc.getPageIndices();
+      const copiedPages = await mergedPdfDoc.copyPages(sourcePdfDoc, sourcePageIndices);
+      for (const page of copiedPages) {
+        mergedPdfDoc.addPage(page);
+      }
+    }
+  
+    const mergedPdfBytes = await mergedPdfDoc.save();
+    const mergedPdfBlob = new Blob([mergedPdfBytes], { type: 'application/pdf' });
+    const downloadLink = document.createElement('a');
+    downloadLink.href = URL.createObjectURL(mergedPdfBlob);
+    downloadLink.download = mergedPdfName;
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+  };  
+  
+
+  // Other event handlers remain unchanged
+
   const handleDragStart = (e: React.DragEvent<HTMLDivElement>, index: number) => {
-    e.dataTransfer.setData('index', index.toString());
+    e.dataTransfer.setData('text/plain', ''); // Required for Firefox
+    draggedItem.current = index;
+    e.dataTransfer.effectAllowed = 'move';
   };
 
   const handleDrop = (e: React.DragEvent<HTMLDivElement>, index: number) => {
     e.preventDefault();
-    const dragIndex = Number(e.dataTransfer.getData('index'));
-    const newPdfs = [...pdfs];
-    const dragItem = newPdfs[dragIndex];
-    newPdfs.splice(dragIndex, 1);
-    newPdfs.splice(index, 0, dragItem);
-    setPdfs(newPdfs);
+    if (draggedItem.current !== null && draggedItem.current !== index) {
+      setPdfs((prevPdfs) => {
+        const newPdfs = [...prevPdfs];
+        const dragItem = newPdfs[draggedItem.current!];
+        newPdfs.splice(draggedItem.current!, 1);
+        newPdfs.splice(index, 0, dragItem);
+        return newPdfs;
+      });
+      setThumbnails((prevThumbnails) => {
+        const newThumbnails = [...prevThumbnails];
+        const dragItem = newThumbnails[draggedItem.current!];
+        newThumbnails.splice(draggedItem.current!, 1);
+        newThumbnails.splice(index, 0, dragItem);
+        return newThumbnails;
+      });
+    }
+    draggedItem.current = null;
   };
 
   const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
+    e.dataTransfer.dropEffect = 'move';
   };
 
   return (
@@ -87,21 +103,21 @@ const PDFHome: React.FC = () => {
           <input type="file" multiple onChange={handlePDFInputChange} />
         </div>
         <div className={styles.pdfList}>
-          {pdfs.map((pdf, index) => (
-            <div
-              key={pdf.name}
-              className={styles.pdfItem}
-              draggable
-              onDragStart={(e) => handleDragStart(e, index)}
-              onDrop={(e) => handleDrop(e, index)}
-              onDragOver={(e) => handleDragOver(e)}
-            >
-              <div className={styles.pdfName}>{pdf.name}</div>
-              <div className={styles.pdfRemove}>
-                <button onClick={() => handleRemovePDF(index)}>X</button>
-              </div>
+        {pdfs && pdfs.map((pdf, index) => (
+          <div
+            key={pdf.name}
+            className={styles.pdfItem}
+            draggable
+            onDragStart={(e) => handleDragStart(e, index)}
+            onDrop={(e) => handleDrop(e, index)}
+            onDragOver={(e) => handleDragOver(e)}
+          >
+            <div className={styles.pdfName}>{pdf.name}</div>
+            <div className={styles.pdfRemove}>
+              <button onClick={() => handleRemovePDF(index)}>X</button>
             </div>
-          ))}
+          </div>
+        ))}
         </div>
         <div className={styles.pdfActions}>
           <button className={styles.pdfButton} onClick={handleSortPDFs}>
@@ -127,6 +143,5 @@ const PDFHome: React.FC = () => {
       </div>
     </>
   );
-};
-
+}
 export default PDFHome;
