673ee025f15f51ffe7a3c4a5418f8544b5893cd9
Ivan <61393936+Sudo-Ivan@users.noreply.github.com>
2023-04-07 23:11:59 -0500

    styling(sidebar): fix responsiveness

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,41 @@
-import { useRouter } from 'next/router';
+import Head from 'next/head';
 import styles from '@/styles/home.module.css';
 
 const Home: React.FC = () => {
-  const router = useRouter();
-
-  const handleNavigateToMerger = () => {
-    router.push('/pdfmerger');
-  };
-
   return (
-    <div className={styles.container}>
-      <h1 className={styles.header}>Welcome to My PDF Tools</h1>
-      <div className={styles.pdfButtonContainer}>
-        <button className={styles.pdfButton} onClick={handleNavigateToMerger}>
-          Merge PDFs
-        </button>
-        <a href="#" download>
-          <button className={styles.downloadAppButton}>Download App</button>
-        </a>
-        <a
-          href="https://github.com/username/my-pdf-tools"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className={styles.githubIcon}></span>
-        </a>
-      </div>
+    <div className={styles.appContainer}>
+      <Head>
+        <title>My PDF Tools</title>
+        <meta name="description" content="A collection of PDF tools" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <header className={styles.header}>
+        <h1>Welcome to My Simple PDF App</h1>
+      </header>
+
+      <main className={styles.main}>
+        <p className={styles.intro}>
+          My PDF Tools is a collection of tools to help you manage and edit your
+          PDF files.
+        </p>
+        <h2 className={styles.toolsHeader}>Available Tools:</h2>
+        <ul className={styles.toolsList}>
+          <li>PDF Merger</li>
+          <li>PDF Splitter</li>
+          <li>PDF to Image</li>
+          <li>Image to PDF</li>
+          <li>Compress PDF</li>
+          <li>Unlock PDF</li>
+        </ul>
+        <div className={styles.pdfButtonContainer}>
+          <a href="#" download>
+            <button className={styles.downloadAppButton}>
+              Download Tauri App
+            </button>
+          </a>
+        </div>
+      </main>
     </div>
   );
 };
