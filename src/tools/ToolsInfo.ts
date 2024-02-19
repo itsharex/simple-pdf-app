1459c05a49cbcb8dfdbdda6db0b1d0ce7d0b7ac0
Ivan Ryan <ivan@ivanryan.dev>
2024-02-19 05:03:31 -0800

    add: toolsinfo.ts

---
+++ src/tools/ToolsInfo.ts
@@ -0,0 +1,11 @@
+// src/tools/ToolsInfo.ts
+const ToolsInfo = [
+  {
+    title: 'PDF Merger Tool',
+    desc: 'A PDF Merger',
+    previewImage: 'https://cdn.statically.io/gh/Sudo-Ivan/MyWebsite-Assets/main/images/website/pdficon01.png',
+    link: '/pdfmerge',
+    metadata: 'pdf merge combine join',
+  },
+];
+export default ToolsInfo;
